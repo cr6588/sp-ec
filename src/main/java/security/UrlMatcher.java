54801bed2a69dfa7f7ecc3f02d84251b9102e5ff
cr6588 <cr6588@vip.qq.com>
2015-12-14 17:09:00 +0800

    init project

---
+++ src/main/java/security/UrlMatcher.java
@@ -0,0 +1,11 @@
+package security;
+
+public interface UrlMatcher {
+    Object compile(String paramString);
+
+    boolean pathMatchesUrl(Object paramObject, String paramString);
+
+    String getUniversalMatchPattern();
+
+    boolean requiresLowerCaseUrl();
+}
